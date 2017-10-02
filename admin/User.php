7bdbdba70d0a3654bb8fe73ce67c05ea3eb232be
leileiwang93 <leilei_8197876@163.com>
2017-10-02 10:08:24 +0800

    Update User.php

--- admin/User.php
+++ admin/User.php
@@ -15,26 +15,19 @@
 	$st = substr($str,0,strlen($str)-1);
 	
 	//获得要查询的QQ号
+	//$time = '';
 	$Qid =  $_GET['new'];
+	$time = $_GET['new2'];
 	
 	//拼接查询sql的查询语句
-	$sql = "select ".$st." from quest_sco where Score_ID=(select Score_ID from quest_man where QQ='$Qid')";
-	
-	
-	//print_r($sql); 
-	//print_r($sql); 
-	
-	//exit(0);
-	
+	$sql = "select ".$st." from quest_sco where Score_ID=(select Score_ID from quest_man where QQ='$Qid' and time='$time')";
 	$result = mysqli_query($link,$sql);
 	
 	$arr = mysqli_fetch_assoc($result);
-	//print_r($arr); 
-	
-	//exit(0);
-	
 	mysqli_close($link);
 	$Q_count = 1;
+	$file = fopen("text.txt","w+"); 
+	
 	
 ?>
 
@@ -48,36 +41,47 @@
 
 <body >
 	
-	<div style="margin:0 auto; text-align: left;width:1000px; position:relative;">
-		<center><h1>评分详情</h1></center>
-		 <?php			
-			for ($i=1; $i<=20; $i++){
-			?>
-			<h4>
-				<span class="subject">原文：<?php echo $i ; ?></span>
-			</h4>
-				<?php
-				for ($j=1; $j<=4; $j++){
-
-		?>
+	<div style="margin:0 auto; text-align: center;width:1000px; position:relative;">
+		<center><h1>QQ号为<?php echo $Qid; ?>的评分详情</h1></center>
 		
-		<div>
+		<div style="margin:0px auto; width:800px; heigth:50px;">
+			<input type=button onclick="window.open('Result.php')" value="返回">
+			<input type=button onclick="window.open('download.php')" value="下载">
+		</div>
 		
-		<div id="item">
-			
-			
-			<p>译文<?php echo $j;?>：&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $arr['Q'.(string)($Q_count++)];?>分</p><br/><br/>
+		<div style="margin:0px auto; width:800px;">
+		<table>
+			<tr>
+				<th width="80px">序号</th>
+				<th width="80px">译文1</th>
+				<th width="80px">译文2</th>
+				<th width="80px">译文3</th>
+				<th width="80px">译文4</th>
 				
+			</tr>
+			
+		<?php			
+			for ($i=1; $i<=20; $i++){
+			?>	
+			<tr>
+				<td>试题<?php echo $i;?></td>
+				<td><?php echo $arr['Q'.(string)($Q_count++)];fwrite($file,$arr['Q'.(string)($Q_count-1)]);?>分</td>
+				<td><?php echo $arr['Q'.(string)($Q_count++)];fwrite($file,$arr['Q'.(string)($Q_count-1)]);?>分</td>
+				<td><?php echo $arr['Q'.(string)($Q_count++)];fwrite($file,$arr['Q'.(string)($Q_count-1)]);?>分</td>
+				<td><?php echo $arr['Q'.(string)($Q_count++)];fwrite($file,$arr['Q'.(string)($Q_count-1)."\\r\\n"]);?>分</td>
+			</td>
 		
-		
-		
-			<?php }}?>	
-			<p><a href="Result.php">返回处理结果页面</a></p>
+		<?php
+			}
+			fclose($file);
+			?>	
+		</table>
 		</div>
-		</div>
+		
 	</div>
 	
 </body>
 
 </html>
+
 
